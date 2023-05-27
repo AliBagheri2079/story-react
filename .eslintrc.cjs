6d0a8e7c3aa977084256cd580754525096cc3f63
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-05-28 02:28:20 +0330

    :wrench: Chore: add eslint tailwind plugin, setup in config file

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,4 +1,5 @@
 module.exports = {
+  root: true,
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
@@ -6,6 +7,7 @@ module.exports = {
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
     'plugin:import/recommended',
+    'plugin:tailwindcss/recommended',
   ],
   overrides: [],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
