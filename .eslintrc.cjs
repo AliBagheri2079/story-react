6c611149923f522d5b5f033f9fb99d2a8f562e66
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-05-28 01:45:21 +0330

    :wrench: Chore: setup eslint import config, create eslint ignore file

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,11 +5,28 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'plugin:import/recommended',
   ],
+  overrides: [],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': 'warn',
+    'import/order': [
+      'warn',
+      {
+        groups: [
+          'type',
+          'object',
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+        ],
+      },
+    ],
   },
-}
+};
