e42a6c08a73d342f225a4b9da31febb36bdb50f1
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-05-28 01:50:56 +0330

    :wrench: Chore: add prettier and setup config, ignore files

---
+++ .prettierrc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  useTabs: false,
+  endOfLine: 'auto',
+  singleAttributePerLine: false,
+  bracketSameLine: false,
+  jsxSingleQuote: true,
+  quoteProps: 'as-needed',
+  semi: true,
+};
