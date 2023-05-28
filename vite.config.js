2c56eda24bd4783a010a5d5bee610594013f2b08
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-05-28 04:03:14 +0330

    :zap:  Perf: setup absolute imports

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, './src') }],
+  },
   plugins: [react()],
-})
+});
