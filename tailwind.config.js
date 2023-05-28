04085e5fdfcff26c6e144b6c782fed7638218ba6
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-05-28 03:31:03 +0330

    :wrench: Chore: setup postcss plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  safelist: [],
+  blocklist: [],
   theme: {
     extend: {},
   },
