decdcc7a12cab7445a7e2e7acdf7e1c1542bbbe4
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-05-30 23:45:39 +0330

    :lipstick: Style: update offcanvas item style

--- src/components/Offcanvas/index.jsx
+++ src/components/Offcanvas/index.jsx
@@ -55,9 +55,9 @@ const Offcanvas = () => {
             '--wave-delay': '700ms',
           }}
         >
-          {/* -translate-y-40 */}
+          {/* -translate-y-full */}
           <ul
-            className={`ms-20 flex h-full flex-col items-start justify-evenly ${Style.List}`}
+            className={`ms-20 flex h-full -translate-y-full flex-col items-start justify-evenly ${Style.List}`}
           >
             <li className='font-moresby text-4xl md:text-5xl lg:text-6xl xl:text-7xl'>
               <NavLink
@@ -83,6 +83,130 @@ const Offcanvas = () => {
                     </span>
                   </span>
                 ))}
+
+                <svg
+                  className='link__graphic link__graphic--slide'
+                  width='300%'
+                  height='100%'
+                  viewBox='0 0 1200 60'
+                  preserveAspectRatio='none'
+                >
+                  <path d='M0,56.5c0,0,298.666,0,399.333,0C448.336,56.5,513.994,46,597,46c77.327,0,135,10.5,200.999,10.5c95.996,0,402.001,0,402.001,0'></path>
+                </svg>
+              </NavLink>{' '}
+              <sup className='duration-500'>5</sup>
+            </li>
+
+            <li className='font-moresby text-4xl md:text-5xl lg:text-6xl xl:text-7xl'>
+              <NavLink
+                href='#item'
+                className='inline-flex overflow-hidden text-inherit'
+              >
+                {'hello world'.split('').map((value, index) => (
+                  <span
+                    key={value}
+                    className='letter letter--reveal relative duration-500 first:capitalize'
+                  >
+                    <span
+                      className='letter--reveal__out inline-flex duration-500'
+                      style={{ transitionDelay: `${index * 25}ms` }}
+                    >
+                      {value}
+                    </span>
+                    <span
+                      className='letter--reveal__in absolute left-0 text-black duration-500'
+                      style={{ transitionDelay: `${index * 25}ms` }}
+                    >
+                      {value}
+                    </span>
+                  </span>
+                ))}
+
+                <svg
+                  className='link__graphic link__graphic--slide'
+                  width='300%'
+                  height='100%'
+                  viewBox='0 0 1200 60'
+                  preserveAspectRatio='none'
+                >
+                  <path d='M0,56.5c0,0,298.666,0,399.333,0C448.336,56.5,513.994,46,597,46c77.327,0,135,10.5,200.999,10.5c95.996,0,402.001,0,402.001,0'></path>
+                </svg>
+              </NavLink>{' '}
+              <sup className='duration-500'>5</sup>
+            </li>
+
+            <li className='font-moresby text-4xl md:text-5xl lg:text-6xl xl:text-7xl'>
+              <NavLink
+                href='#item'
+                className='inline-flex overflow-hidden text-inherit'
+              >
+                {'hello world'.split('').map((value, index) => (
+                  <span
+                    key={value}
+                    className='letter letter--reveal relative duration-500 first:capitalize'
+                  >
+                    <span
+                      className='letter--reveal__out inline-flex duration-500'
+                      style={{ transitionDelay: `${index * 25}ms` }}
+                    >
+                      {value}
+                    </span>
+                    <span
+                      className='letter--reveal__in absolute left-0 text-black duration-500'
+                      style={{ transitionDelay: `${index * 25}ms` }}
+                    >
+                      {value}
+                    </span>
+                  </span>
+                ))}
+
+                <svg
+                  className='link__graphic link__graphic--slide'
+                  width='300%'
+                  height='100%'
+                  viewBox='0 0 1200 60'
+                  preserveAspectRatio='none'
+                >
+                  <path d='M0,56.5c0,0,298.666,0,399.333,0C448.336,56.5,513.994,46,597,46c77.327,0,135,10.5,200.999,10.5c95.996,0,402.001,0,402.001,0'></path>
+                </svg>
+              </NavLink>{' '}
+              <sup className='duration-500'>5</sup>
+            </li>
+
+            <li className='font-moresby text-4xl md:text-5xl lg:text-6xl xl:text-7xl'>
+              <NavLink
+                href='#item'
+                className='inline-flex overflow-hidden text-inherit'
+              >
+                {'hello world'.split('').map((value, index) => (
+                  <span
+                    key={value}
+                    className='letter letter--reveal relative duration-500 first:capitalize'
+                  >
+                    <span
+                      className='letter--reveal__out inline-flex duration-500'
+                      style={{ transitionDelay: `${index * 25}ms` }}
+                    >
+                      {value}
+                    </span>
+                    <span
+                      className='letter--reveal__in absolute left-0 text-black duration-500'
+                      style={{ transitionDelay: `${index * 25}ms` }}
+                    >
+                      {value}
+                    </span>
+                  </span>
+                ))}
+
+                <svg
+                  className='link__graphic link__graphic--slide'
+                  width='300%'
+                  height='100%'
+                  viewBox='0 0 1200 60'
+                  preserveAspectRatio='none'
+                >
+                  <path d='M0,56.5c0,0,298.666,0,399.333,0C448.336,56.5,513.994,46,597,46c77.327,0,135,10.5,200.999,10.5c95.996,0,402.001,0,402.001,0'></path>
+                </svg>
               </NavLink>{' '}
               <sup className='duration-500'>5</sup>
             </li>
