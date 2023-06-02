ebdb8c30696674caee24fee2fc544fec902284e2
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-06-02 14:50:07 +0330

    :wrench: Chore: add svgr plugin in vite config file

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import react from '@vitejs/plugin-react-swc';
+import svgr from 'vite-plugin-svgr';
 import { defineConfig } from 'vite';
 import path from 'path';
 
@@ -7,5 +8,28 @@ export default defineConfig({
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, './src') }],
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    svgr({
+      // Set it to `true` to export React component as default.
+      // Notice that it will override the default behavior of Vite.
+      exportAsDefault: false,
+
+      // svgr options: https://react-svgr.com/docs/options/
+      svgrOptions: {
+        // ...
+      },
+
+      // esbuild options, to transform jsx to js
+      esbuildOptions: {
+        // ...
+      },
+
+      //  A minimatch pattern, or array of patterns, which specifies the files in the build the plugin should include. By default all svg files will be included.
+      include: '**/*.svg',
+
+      //  A minimatch pattern, or array of patterns, which specifies the files in the build the plugin should ignore. By default no files are ignored.
+      exclude: '',
+    }),
+  ],
 });
