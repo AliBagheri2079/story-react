1460e89f6b7355e89f0ccaea4b4d703c2fdedbb3
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-06-02 14:53:04 +0330

    :sparkles: Feat: create svg component instance

---
+++ src/components/Icons/instance/index.jsx
@@ -0,0 +1,56 @@
+import PropTypes from 'prop-types';
+
+const child = ({ name = '', ...props }) => {
+  switch (name) {
+    case 'Circle':
+      return <circle {...props} />;
+
+    case 'Rectangle':
+      return <rect {...props} />;
+
+    default:
+      return <path {...props} />;
+  }
+};
+
+child.propTypes = {
+  name: PropTypes.oneOf(['Circle', 'Rectangle', 'Path']).isRequired,
+  props: PropTypes.any,
+};
+
+const Svg = ({
+  id,
+  className,
+  style,
+  width = '100',
+  height = '100',
+  viewBox = '',
+  children,
+}) => (
+  <svg
+    id={id}
+    className={className}
+    style={style}
+    width={width}
+    height={height}
+    viewBox={viewBox}
+    xmlns='http://www.w3.org/2000/svg'
+    xmlnsXlink='http://www.w3.org/1999/xlink'
+  >
+    {children}
+  </svg>
+);
+
+Svg.propTypes = {
+  id: PropTypes.string,
+  className: PropTypes.string,
+  width: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  height: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  style: PropTypes.objectOf(PropTypes.object),
+  viewBox: PropTypes.string,
+  children: PropTypes.element,
+};
+
+Svg.Child = child;
+
+export default Svg;
