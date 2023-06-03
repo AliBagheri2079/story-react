e4c3edec538abda5cef1cfad47003b9a2616261b
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-06-03 03:44:19 +0330

    :lipstick: Style: update header title style

--- src/components/HeaderTitle/index.jsx
+++ src/components/HeaderTitle/index.jsx
@@ -3,8 +3,8 @@ import { Title } from './index.stitches';
 
 const HeaderTitle = () => {
   return (
-    <div className='flex h-full justify-center'>
-      <Title className='font-monument-ultrabold line-clamp-6 h-auto w-full text-center'>
+    <div className='flex h-full items-center'>
+      <Title className='font-monument-ultrabold line-clamp-6 text-center'>
         Lorem <CurveUnderline>curve</CurveUnderline> sit amet{' '}
         <WaveUnderline>wave</WaveUnderline> adipisicing
       </Title>
