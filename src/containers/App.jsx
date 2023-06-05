0212f678f0217eafd35bbc730ab56815e855cb5d
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-06-05 22:08:34 +0330

    :sparkles: Feat: create gsap context, initialize on container file

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,10 +1,12 @@
+import GsapContext from '@/context/Gsap';
+import useGsapContext from '@/hooks/useGsapContext';
 import Home from '@/pages/Home.jsx';
 
 const App = () => {
   return (
-    <>
+    <GsapContext.Provider value={{ useGsapContext }}>
       <Home />
-    </>
+    </GsapContext.Provider>
   );
 };
 
